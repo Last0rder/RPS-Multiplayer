5d8a39ec1092a76bdb37ac5db75d7dec8da8d927
Nate Shih <nshih.85@gmail.com>
2016-09-17 10:17:25 -0400

    Initial Commit

---
+++ assets/java/app.js
@@ -0,0 +1,11 @@
+ // Firebase Initialization
+
+ var config = {
+    apiKey: "AIzaSyDVWpDQ_V0TeOgf5OmkDgdwMr5G6lwSVAA",
+    authDomain: "rcb-rps.firebaseapp.com",
+    databaseURL: "https://rcb-rps.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "235476200662"
+  };
+  firebase.initializeApp(config);
+
